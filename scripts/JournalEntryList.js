04537f11097385953dc6821de1ab0456fd07327f
Cornelia Johnson <corneliaj91@gmail.com>
2020-08-31 16:53:36 -0400

    changed to map function

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -4,19 +4,18 @@
  *    there are items in the collection exposed by the
  *    data provider component
  */
-import { useJournalEntries } from "./JournalDataProvider.js"
-import { JournalEntryComponent } from "./JournalEntry.js"
+import { useJournalEntries } from "./JournalDataProvider.js";
+import { JournalEntryComponent } from "./JournalEntry.js";
 
 // DOM reference to where all entries will be rendered
-const entryLog = document.querySelector("#entryLog")
+const entryLog = document.querySelector("#entryLog");
 
 export const EntryListComponent = () => {
   // Use the journal entry data from the data provider component
-  const entries = useJournalEntries()
+  const entries = useJournalEntries();
+  entryLog.innerHTML += `${entries
+    .map((entry) => JournalEntryComponent(entry))
+    .join("")}`;
 
-  for (const entry of entries) {
-    entryLog.innerHTML += JournalEntryComponent(entry)
-  }
-
-  return entryLog
-}
+  return entryLog;
+};
