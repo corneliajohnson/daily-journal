28ddfbdaac99bfbee4cbaed11547ce5babf5e6f9
Cornelia Johnson <corneliaj91@gmail.com>
2020-09-18 12:20:38 -0400

    added css to journal entry

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -5,15 +5,18 @@
 export const JournalEntryComponent = (entry) => {
   return `
       <section id="entry--${entry.id}" class="journalEntry">
+      <div>
           <h5>${entry.entryTopic}</h5>
           <p>${new Date(entry.entryDate).toDateString()}: ${
     entry.mood.label
   }</p>
           <p class="entry--body">${entry.entryText}</p>
-          <button type="button" class="btn" id="deleteEntry--${
+          </div>
+          <div>
+          <button type="button" class="btn red" id="deleteEntry--${
             entry.id
           }">Delete</button>
-          <hr>
+          </div>
       </section>
   `;
 };
